2efbcc7af8c3f7495e5e5476aab257151ef6bd05
Tatiana <sutherland.tati@gmail.com>
2017-02-24 21:22:31 -0500

    trying to center chair

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -8,7 +8,7 @@ const DumbHome = () => (
     <div>
         <section>
             <h1 className='red'>HOT <span id="notRed">SEAT</span></h1>
-            <div className="chair"><img src="cropped-chair.png" height="150" width="150"></img></div>
+            <div className="chair" style="will-change: transform; transform: translateX(calc(50vw - 50%);"><img src="cropped-chair.png" height="150" width="150"></img></div>
         </section>
     </div>
 )
@@ -20,7 +20,7 @@ class Home extends Component {
   componentDidMount() {
     anime({
       targets: '.chair',
-      translateX: calc(50vw - 50%);
+      translateX: 500,
       scale: [.75, .9],
       delay: function(el, index) {
         return index * 80;
