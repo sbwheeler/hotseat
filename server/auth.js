925905a22d7ca2f76bd6e46fd2133c2a64d6871f
Tatiana <sutherland.tati@gmail.com>
2017-03-02 21:33:02 -0500

    getting token

--- server/auth.js
+++ server/auth.js
@@ -1,18 +1,16 @@
 'use-strict'
 const app = require('APP'), {env} = app
 const debug = require('debug')(`${app.name}:auth`)
-const request = require('request')
+// const request = require('request')
+const axios = require('axios')
 
 // const User = require('APP/db/models/user')
 const auth = require('express').Router()
 
 // POST requests for local login:
 auth.post('/login/learndot', (req, res) => {
-    console.log('email and password:::::', req.body)
-    request.post('https://learn.fullstackacademy.com/auth/local', req.body, (err, res, body) => {
-        if (err) console.error(err)
-        console.log('BODY:::::', body)
-    }) 
+    axios.post('https://learn.fullstackacademy.com/auth/local', req.body)
+    .then(console.log, console.error)
 })
 
 auth.post('/logout', (req, res, next) => {
